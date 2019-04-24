d39aa5e346a487b8a2f9433219e0e2263a9906d0
Dmytro <ddmutro2@gmail.com>
2019-04-24 14:55:21 +0300

    Initial commit.

---
+++ MyCV/MyCV/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyCV
+//
+//  Created by Dima Dobrovolskyy on 4/24/19.
+//  Copyright © 2019 Dima Dobrovolskyy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
